e4687cc0de147d264fbf7e99bf3a7a3c06c15d4d
Dariusz Palt <246808@student.pwr.edu.pl>
2020-06-10 20:44:20 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+from utils import mnist_reader
+import numpy as np
+
+
+X_train, y_train = mnist_reader.load_mnist('data/fashion', kind='train')
+X_test, y_test = mnist_reader.load_mnist('data/fashion', kind='t10k')
