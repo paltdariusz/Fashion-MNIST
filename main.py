8dae57042359e4646368272b55e4807bc059e03e
Dariusz Palt <246808@student.pwr.edu.pl>
2020-06-13 22:23:48 +0200

    load & normalize images and predict using knn

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-from utils import mnist_reader
 import numpy as np
 
+from utils import mnist_reader
+import content as ct
 
 X_train, y_train = mnist_reader.load_mnist('data/fashion', kind='train')
 X_test, y_test = mnist_reader.load_mnist('data/fashion', kind='t10k')
+
+k = [5, 7]
+
+X_train_n, X_test_n = X_train / 255, X_test / 255
+
+error, best_k, errors = ct.model_selection_knn(X_test_n, X_train_n, y_test, y_train, k)
+
+print(error, best_k, errors)
